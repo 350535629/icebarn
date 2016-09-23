f039a742252b7f171b747b99b4098e1752ca16d6
Baymax Chuang <350535629@qq.com>
2016-09-22 23:20:03 -0500

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,7 @@
+#The book says that the name of "#" is "octothorpe" or 'pound character'
+#Alse the boos says anything after the # would be ignored by python
+
+print 'I want to try that if I insert # in this sentence \nwhat will happen'
+#2333print 'Helloworld'
+
+print'This will run'
